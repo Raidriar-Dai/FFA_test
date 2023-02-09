c8d25499f7c959321a020e960dad1b5faded2e89
Qirun Dai <qirundai@sina.com>
2023-02-08 19:30:12 -0800

    implement local receptive field by using a mask

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # FFA_test
 - Code modified from **mohammadpz/pytorch_forward_forward**.
-- Added implementation on CIFAR-10, with additional weight-decay and dropout.
-- Incorporated wandb sweep functionality into original codes.
-- Updated config files, and updated reports in Feishu Documents.
+- Added implementation on CIFAR-10, with additional weight-decay and dropout layer.
+- Incorporated wandb sweep functionality into original codes by using config files.
+- Modified training pipeline: training one layer for 1000 epochs, each epoch containing 10 minibatches, then progressing into the following layer (and will not return to the previous layer later on)
+- Published a branch called **"local_receptive"**, where a technique of local receptive field was implemented in order to reduce overfitting (according to Hinton's FFA13 paper Chapter 4)
--- linear_layer.py
+++ linear_layer.py
@@ -6,7 +6,7 @@ from torch.optim import Adam, AdamW
 # The simplest linear layer, without dropout or weight-decay
 class Layer(nn.Linear):
     def __init__(self, in_features, out_features, lr, threshold,
-                 bias=True, device=None, dtype=None):
+                bias=True, device=None, dtype=None):
         super().__init__(in_features, out_features, bias, device, dtype)
         self.relu = torch.nn.ReLU()
         self.opt = Adam(self.parameters(), lr=lr)
@@ -15,10 +15,10 @@ class Layer(nn.Linear):
     def forward(self, x):
         # Under L-2 based goodness: normalize the input x
         x_direction = x / (x.norm(2, 1, keepdim=True) + 1e-4)
-        
+
         # # Under L-1 based goodness: normalize the input x
         # x_direction = x / (x.norm(1, 1, keepdim=True) + 1e-4)
-        
+
         return self.relu(
             torch.mm(x_direction, self.weight.T) +
             self.bias.unsqueeze(0))
@@ -29,35 +29,63 @@ class Layer(nn.Linear):
         # The following goodness is L-2 based:
         g_pos = self.forward(x_pos.detach()).pow(2).mean(1)
         g_neg = self.forward(x_neg.detach()).pow(2).mean(1)
-        
+
         # # The following goodness is L-1 based:
         # g_pos = self.forward(x_pos.detach()).abs().mean(1)
         # g_neg = self.forward(x_neg.detach()).abs().mean(1)
-        
+
         # The following loss pushes pos (neg) samples to values larger (smaller) than the self.threshold.
         loss = torch.log(1 + torch.exp(torch.cat([
             -g_pos + self.threshold,
             g_neg - self.threshold]))).mean()   # calculate the mean of both positive and negative samples
-        
+
         # # The following loss pushes pos (neg) samples to values smaller (larger) than the self.threshold.
         # loss = torch.log(1 + torch.exp(torch.cat([
         #     g_pos - self.threshold,
-        #     -g_neg + self.threshold]))).mean() 
-        
+        #     -g_neg + self.threshold]))).mean()
+
         self.opt.zero_grad()
-        # This backward just compute the derivative and hence is not considered backpropagation.
         loss.backward()
-        # loss.backward(retain_graph=True)
         self.opt.step()
-        
+
         # layer.train() 不再需要返回值
 
 
 # Linear layer with weight-decay, default to 0.01
 class CIFAR_Layer(Layer):
-    # Override INIT method in Layer in order to implement weight_decay
+    # Override INIT method in Layer in order to implement <weight_decay> and <local_receptive_field>.
     def __init__(self, in_features, out_features,
-                 lr, threshold, weight_decay,
-                 bias=True, device=None, dtype=None):
+                lr, threshold, weight_decay, mask,
+                bias=True, device=None, dtype=None):
         super().__init__(in_features, out_features, lr, threshold, bias, device, dtype)
         self.opt = AdamW(self.parameters(), lr=lr, weight_decay=weight_decay)
+        self.mask = mask
+
+    # 重写 train 方法, 在每次参数更新之后, 给参数矩阵套上蒙板(mask), 再用修正后的参数矩阵进行下一次训练
+    def train(self, x_pos, x_neg):
+        # with torch.enable_grad():
+
+        # The following goodness is L-2 based:
+        g_pos = self.forward(x_pos.detach()).pow(2).mean(1)
+        g_neg = self.forward(x_neg.detach()).pow(2).mean(1)
+
+        # # The following goodness is L-1 based:
+        # g_pos = self.forward(x_pos.detach()).abs().mean(1)
+        # g_neg = self.forward(x_neg.detach()).abs().mean(1)
+
+        # The following loss pushes pos (neg) samples to values larger (smaller) than the self.threshold.
+        loss = torch.log(1 + torch.exp(torch.cat([
+            -g_pos + self.threshold,
+            g_neg - self.threshold]))).mean()   # calculate the mean of both positive and negative samples
+
+        # # The following loss pushes pos (neg) samples to values smaller (larger) than the self.threshold.
+        # loss = torch.log(1 + torch.exp(torch.cat([
+        #     g_pos - self.threshold,
+        #     -g_neg + self.threshold]))).mean()
+
+        self.opt.zero_grad()
+        loss.backward()
+        self.opt.step()
+
+        with torch.no_grad():
+            self.weight.masked_fill_(self.mask, 0)
